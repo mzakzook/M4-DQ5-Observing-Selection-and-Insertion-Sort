baf6fa91f0447a05593e2762b5d4151d4a0c6f76
Max Zakzook <mzakzook@me.com>
2019-11-25 09:49:46 -0800

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,15 @@
-function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
-}
+let insertionSort = (inputArr) => {
+    let length = inputArr.length;
+    for (let i = 1; i < length; i++) {
+        let key = inputArr[i];
+        let j = i - 1;
+        while (j >= 0 && inputArr[j] > key) {
+            inputArr[j + 1] = inputArr[j];
+            j = j - 1;
+        }
+        inputArr[j + 1] = key;
+    }
+    return inputArr;
+};
+
+console.log(insertionSort([5,2,3,6]))
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,19 @@
-function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let selectionSort = (arr) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr;
 }
+
+console.log(selectionSort([5, 2, 3, 6]))
